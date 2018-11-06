e44f62ea1d85a53cf3da13fcd9330a3c92501054
AndersonMontor <ander_1434@hotmail.com>
2018-11-06 01:11:08 -0200

    timeout e retransmissao

--- server_e2.py
+++ server_e2.py
@@ -11,6 +11,7 @@ import socket
 import struct
 import os
 import random
+import time
 
 FLAGS_FIN = 1<<0
 FLAGS_SYN = 1<<1
@@ -20,6 +21,7 @@ FLAGS_ACK = 1<<4
 MSS = 1460
 
 TESTAR_PERDA_ENVIO = False
+VERBOSE = 1
 
 
 class Conexao:
@@ -30,15 +32,31 @@ class Conexao:
         self.send_queue = b"HTTP/1.0 200 OK\r\nContent-Type: text/plain\r\n\r\n"
         for i in range(1000):
             self.send_queue += b"ABCDEFGHIJKLMNO(%d)\n" % i
+
+        self.SRTTackno = None
+        self.SRTTstime = None
+        self.estimatedRTT = 0
+        self.devRTT = 0
+        self.timeoutInterval = 1000 #1 segundo
+        self.nonACKs = {}
+        self.timerRunning = False
+
+
 conexoes = {}
 
 
+def print_verbose(text, verbose_level):
+    if VERBOSE >= verbose_level:
+        print(text)
 
 def addr2str(addr):
     return '%d.%d.%d.%d' % tuple(int(x) for x in addr)
 
 def str2addr(addr):
     return bytes(int(x) for x in addr.split('.'))
+
+def time2ms(timestamp):
+    return (timestamp * 1000)
 
 def handle_ipv4_header(packet):
     version = packet[0] >> 4
@@ -79,6 +97,20 @@ def fix_checksum(segment, src_addr, dst_addr):
     return bytes(seg)
 
 
+def resend(fd, conexao):
+    if conexao.nonACKs.keys():  
+        re_ackno = min(conexao.nonACKs.keys())
+        if VERBOSE >= 1:
+            print("-- Resending: %d" % re_ackno)
+
+        segment, dst_addr, dst_port = conexao.nonACKs[re_ackno]
+        fd.sendto(segment, (dst_addr, dst_port))
+        asyncio.get_event_loop().call_later(conexao.timeoutInterval, resend, fd, conexao)
+        conexao.timerRunning = True
+
+        #Dobra o timeout temporariamente
+        conexao.timeoutInterval *= 2
+
 def send_next(fd, conexao):
     payload = conexao.send_queue[:MSS]
     conexao.send_queue = conexao.send_queue[MSS:]
@@ -91,10 +123,29 @@ def send_next(fd, conexao):
 
     conexao.seq_no = (conexao.seq_no + len(payload)) & 0xffffffff
 
+
     segment = fix_checksum(segment, src_addr, dst_addr)
 
     if not TESTAR_PERDA_ENVIO or random.random() < 0.95:
         fd.sendto(segment, (dst_addr, dst_port))
+        if VERBOSE >= 1:
+            print("Sending: %d" % conexao.seq_no)
+
+        #Anota o tempo para calculo do RTT no recebimento do ACK
+        if not conexao.SRTTstime:
+            conexao.SRTTstime = time.time()
+            conexao.SRTTackno = conexao.seq_no
+
+        #Coloca na lista de nao-ackeds e roda o timer
+        conexao.nonACKs[conexao.seq_no] = (segment, dst_addr, dst_port)
+
+        #Calculo do timeout
+        conexao.timeoutInterval = conexao.estimatedRTT + (4 * conexao.devRTT)
+
+        if not conexao.timerRunning:
+            asyncio.get_event_loop().call_later(conexao.timeoutInterval, resend, fd, conexao)
+            conexao.timerRunning = True
+
 
     if conexao.send_queue == b"":
         segment = struct.pack('!HHIIHHHH', src_port, dst_port, conexao.seq_no,
@@ -120,25 +171,24 @@ def raw_recv(fd):
 
     calc_check, = struct.unpack("!H", fix_checksum(segment, src_addr, dst_addr)[16:18])
 
-
     #Droppa o pacote se o checksum não bater, está comentado devido a mal funcionamento quando executado localmente
 
     # if (checksum != calc_check):
     #     return
     
-
-    print ("Src addr: ", src_addr)
-    print ("Dst addr: ", dst_addr)
-    print ("Src port: ", src_port)
-    print ("Dst port: ", dst_port)
-    print ("Sequence number: ", seq_no)
-    print ("Ack number: ", ack_no)
-    print ("Flags: ", flags)
-    print ("Window size: ", window_size)
-    print ("Urgent pointer: ", urg_ptr)
-    print ("Checksum: ", checksum)
-    print("Calculated checksum: ",  calc_check)
-    print()
+    if VERBOSE >= 2:
+        print ("Src addr: ", src_addr)
+        print ("Dst addr: ", dst_addr)
+        print ("Src port: ", src_port)
+        print ("Dst port: ", dst_port)
+        print ("Sequence number: ", seq_no)
+        print ("Ack number: ", ack_no)
+        print ("Flags: ", flags)
+        print ("Window size: ", window_size)
+        print ("Urgent pointer: ", urg_ptr)
+        print ("Checksum: ", checksum)
+        print("Calculated checksum: ",  calc_check)
+        print()
 
     payload = segment[4*(flags>>12):]
 
@@ -157,9 +207,51 @@ def raw_recv(fd):
         conexao.seq_no += 1
 
         asyncio.get_event_loop().call_later(.1, send_next, fd, conexao)
+
     elif id_conexao in conexoes:
         conexao = conexoes[id_conexao]
         conexao.ack_no += len(payload)
+
+        if (flags & FLAGS_ACK) == FLAGS_ACK:
+            if VERBOSE >= 1:
+                print ("ACK received:", ack_no)
+
+            #Calculo do RTT baseado no tempo anotado do pacote correspondente a este ACK
+            if conexao.SRTTackno and (ack_no == (conexao.SRTTackno)):
+                sampleRTT = time.time() - conexao.SRTTstime
+                #Calculo media dos RTTs
+                if conexao.estimatedRTT:
+                    conexao.estimatedRTT = 0.875 * conexao.estimatedRTT + 0.125 * sampleRTT
+                else:
+                    conexao.estimatedRTT = sampleRTT
+
+                #Calculo variância dos RTTs
+                conexao.devRTT = (0.75 * conexao.devRTT) + (0.25 * abs(sampleRTT - conexao.estimatedRTT))
+
+                conexao.SRTTackno = conexao.SRTTstime = None
+
+                #Calculo do timeout
+                conexao.timeoutInterval = conexao.estimatedRTT + (4 * conexao.devRTT)
+
+                if VERBOSE >= 2:
+                    print ("SampleRTT:", sampleRTT)
+                    print ("EstimatedRTT", conexao.estimatedRTT)
+                    print ("DevRTT", conexao.devRTT)
+                    print ("Timeout", conexao.timeoutInterval)
+
+            #Remove da lista de nao-ackeds os que foram acked agora
+            for acked in [x for x in conexao.nonACKs.keys() if x <= ack_no]:
+                del conexao.nonACKs[acked]
+
+            #Se tiver algum nao-acked ainda, roda o timer
+            if len(conexao.nonACKs.keys()):
+                asyncio.get_event_loop().call_later(conexao.timeoutInterval, resend, fd, conexao)
+                conexao.timerRunning = True
+            else:
+                conexao.timerRunning = False
+
+
+
     else:
         print('%s:%d -> %s:%d (pacote associado a conexão desconhecida)' %
             (src_addr, src_port, dst_addr, dst_port))
